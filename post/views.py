ead00bc07d1eab1deb5f68e140b9ffdf848adc31
Kaung Myat Han <arkarhtethan@gmail.com>
2019-07-03 23:39:17 +0630

    added tag functionality to our platform

--- post/views.py
+++ post/views.py
@@ -1,8 +1,20 @@
 from django.shortcuts import render
 from django.views import generic
-
+from .models import Post, Tag
 # Create your views here.
 
-class BlogHome(generic.TemplateView):
+class BlogHome(generic.ListView):
+
+    model = Post
+
+    paginate_by = 4
 
     template_name = 'post/home.html'
+
+    def get_context_data(self):
+
+        ctx = super().get_context_data()
+
+        ctx['tags'] = Tag.objects.all()
+
+        return ctx
